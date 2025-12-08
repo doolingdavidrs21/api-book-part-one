4cb3b905c2aa1255f19e310cf4873d5dd473bf45
doolingdavidrs21 <david@rs21.io>
2025-12-08 14:53:41 -0700

    added chapter7/chapter7notes.ipynb

--- chapter7/sdk/src/swcpy/swc_client.py
+++ chapter7/sdk/src/swcpy/swc_client.py
@@ -1,7 +1,38 @@
 import httpx
+import swcpy.swc_config as config
+from .schemas import League, Team, Player, Counts, Performance
+from typing import List
+import backoff
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class SWCClint:
+    """Interacts with the SportsWorldCentral API.
+
+    This SDK class simplifies the process of using the SWC
+    Fantasty Football API. It supports all the functions of the
+    SWC API and returns validated data types.
+
+    Typical usage example:
+
+        swc = SWCClient()
+        response = client.get_health_check()
+    """
+
+    HEALTH_CHECK_ENDPOINT = "/"
+    LIST_LEAGUES_ENDPOINT = "/v0/leagues/"
+    LIST_PLAYERS_ENDPOINT = "/v0/players/"
+    LIST_TEAMS_ENDPOINT = "/v0/teams/"
+    LIST_PERFORMANCES_ENDPOINT = "/v0/performances/"
+    LIST_COUNTS_ENDPOINT = "/v0/counts/"
+
+    BULK_FILE_BASE_URL = (
+        "https://raw.githubusercontent.com/doolingdavidrs21"
+        + "/api-book-part-one/main/bulk/"
+    )
+
     def __init__(self, swc_base_url: str):
         self.swc_base_url = swc_base_url
 
